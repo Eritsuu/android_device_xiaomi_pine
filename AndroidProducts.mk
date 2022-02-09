01d3aa77cce9172e6378d8372438e92820325db0
vytautas <vytautas.duchas@gmail.com>
2022-02-09 21:52:42 +0200

    welcome evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_pine-user \
-    aosp_pine-userdebug \
-    aosp_pine-eng
+    evolution_pine-user \
+    evolution_pine-userdebug \
+    evolution_pine-eng
